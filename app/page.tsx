2da0916bd3e9a272f16c7dd166800ccad9a4785b
Andrew Paige <keeperpaige@gmail.com>
2025-02-13 17:50:22 -0500

    revert back to server side

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-"use client"
-
-import { useUser } from '@auth0/nextjs-auth0/client';
+import { getSession } from "@auth0/nextjs-auth0";
 import LandingPage from '../components/LandingPage';
 import HomePage from '../components/HomePage';
 import Menu from '../components/Menu';
 
+export default async function Index() {
+  const session = await getSession();
 
-export default  function Index() {
-  const { user, isLoading } = useUser();
-
-
-  if (isLoading) return
   // If user is not authenticated, show landing page
-  if (!user) {
+  if (!session) {
     return (
       <>
         <Menu />
