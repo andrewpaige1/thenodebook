42a4b0b9275b2cd2b79ba46712917d6ed2d7f427
Andrew Paige <keeperpaige@gmail.com>
2025-09-20 16:32:23 -0400

    base url

--- services/authService.ts
+++ services/authService.ts
@@ -1,5 +1,5 @@
 export async function fetchAccessToken(): Promise<string> {
-  const baseUrl = process.env.APP_BASE_URL!;
+  const baseUrl = process.env.NEXT_PUBLIC_APP_BASE_URL!;
   try {
     const res = await fetch(`${baseUrl}/api/auth/token/`, {
       method: "GET"
@@ -9,7 +9,7 @@ export async function fetchAccessToken(): Promise<string> {
      // console.error(`Failed to fetch access token: HTTP ${res.status} - ${res.statusText}`);
      // console.error(`Response body: ${errorText}`);
       //throw new Error(`Failed to fetch access token: ${res.status} ${res.statusText}`);
-      return ''
+      return `${JSON.stringify(errorText)}`
     }
     const data = await res.json();
     if (!data.accessToken) {
