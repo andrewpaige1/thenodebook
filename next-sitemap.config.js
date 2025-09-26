7fb461c70f9c7db4697a5e01b9c62297a61ebef2
Andrew Paige <keeperpaige@gmail.com>
2025-09-26 18:18:14 -0400

    sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,27 @@
 /** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://mindthred.com',
+  siteUrl: 'https://mindthred.com',
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
   generateRobotsTxt: true,
+  // Default transformation function
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
 }
