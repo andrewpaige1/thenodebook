b9f0019daa7de1230226444dcca3c1c262622a3f
Andrew Paige <keeperpaige@gmail.com>
2025-09-26 18:22:17 -0400

    new sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,16 +6,6 @@ module.exports = {
   priority: 0.7,
   sitemapSize: 5000,
   generateRobotsTxt: true,
-  // Default transformation function
-  transform: async (config, path) => {
-    return {
-      loc: path,
-      changefreq: config.changefreq,
-      priority: config.priority,
-      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-      alternateRefs: config.alternateRefs ?? [],
-    }
-  },
   robotsTxtOptions: {
     policies: [
       {
